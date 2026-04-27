fc443d4232c2fdf965a8fbd18bef36df6741186b
Max Max <teardbro@gmail.com>
2026-04-27 13:44:18 +0200

    закончил Tabs-Blog

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import ElementObserverCollections from './ElementObserver.js'
 import Header from './Header.js'
-
+import TabsCollection from './Tabs.js'
 
 new ElementObserverCollections()
 new Header()
+new TabsCollection()
