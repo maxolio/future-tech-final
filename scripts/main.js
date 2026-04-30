c67270ffa6d89248932da0a72807cbd90cf643ee
Max Max <teardbro@gmail.com>
2026-04-30 19:30:47 +0200

    закончил News Video

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,9 @@
 import ElementObserverCollections from './ElementObserver.js'
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
+import VideoPlayerCollection from './VideoPlayer.js'
 
 new ElementObserverCollections()
 new Header()
 new TabsCollection()
+new VideoPlayerCollection()
