58030f5f669e77201f9b4afe305b9552bb805feb
Max Max <teardbro@gmail.com>
2026-05-02 16:55:17 +0200

    закончил Blog

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,10 @@ import ElementObserverCollections from './ElementObserver.js'
 import Header from './Header.js'
 import TabsCollection from './Tabs.js'
 import VideoPlayerCollection from './VideoPlayer.js'
+import ExpandableContentCollection from './Expandable-content.js'
 
 new ElementObserverCollections()
 new Header()
 new TabsCollection()
 new VideoPlayerCollection()
+new ExpandableContentCollection()
